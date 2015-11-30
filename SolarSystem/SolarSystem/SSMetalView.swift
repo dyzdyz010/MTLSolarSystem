046b484d6909af1f9ac9198453a58c9964e7b166
Du Yizhuo <dyzdyz010@sina.com>
2015-11-30 22:12:51 +0800

    Prettify

--- SolarSystem/SolarSystem/SSMetalView.swift
+++ SolarSystem/SolarSystem/SSMetalView.swift
@@ -10,12 +10,12 @@ import UIKit
 import simd
 
 class SSMetalView: UIView {
-    var _device: MTLDevice! = nil
-    var _metalLayer: CAMetalLayer! = nil
-    var _vertexBuffer: MTLBuffer! = nil
+    var _device: MTLDevice!                     = nil
+    var _metalLayer: CAMetalLayer!              = nil
+    var _vertexBuffer: MTLBuffer!               = nil
     var _pipelineState: MTLRenderPipelineState! = nil
-    var _commandQueue: MTLCommandQueue! = nil
-
+    var _commandQueue: MTLCommandQueue!         = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -25,11 +25,11 @@ class SSMetalView: UIView {
     }
     
     func setupMetal() {
-        _device = MTLCreateSystemDefaultDevice()
-        _metalLayer = CAMetalLayer()
-        _metalLayer.device = _device
-//        _metalLayer.frame = self.frame
-        _metalLayer.pixelFormat = MTLPixelFormat.BGRA8Unorm
+        _device                     = MTLCreateSystemDefaultDevice()
+        _metalLayer                 = CAMetalLayer()
+        _metalLayer.device          = _device
+        //        _metalLayer.frame = self.frame
+        _metalLayer.pixelFormat     = MTLPixelFormat.BGRA8Unorm
         _metalLayer.framebufferOnly = true
         
         self.layer.addSublayer(_metalLayer)
@@ -37,12 +37,12 @@ class SSMetalView: UIView {
     
     func makePipeline() {
         let lib = _device.newDefaultLibrary()!
-        let vertexFunc = lib.newFunctionWithName("vertexMain")
+        let vertexFunc   = lib.newFunctionWithName("vertexMain")
         let fragmentFunc = lib.newFunctionWithName("fragmentMain")
         
-        let renderPipelineDesc = MTLRenderPipelineDescriptor()
-        renderPipelineDesc.vertexFunction = vertexFunc
-        renderPipelineDesc.fragmentFunction = fragmentFunc
+        let renderPipelineDesc                             = MTLRenderPipelineDescriptor()
+        renderPipelineDesc.vertexFunction                  = vertexFunc
+        renderPipelineDesc.fragmentFunction                = fragmentFunc
         renderPipelineDesc.colorAttachments[0].pixelFormat = MTLPixelFormat.BGRA8Unorm
         
         do {
@@ -57,8 +57,8 @@ class SSMetalView: UIView {
     
     func makeBuffers() {
         let triangle = [SSVertex(position: vector_float4(0, 0.5, 0, 1), color: vector_float4(1.0, 0.0, 0.0, 1.0)),
-        SSVertex(position: vector_float4(-0.5, -0.5, 0, 1), color: vector_float4(0.0, 1.0, 0.0, 1.0)),
-        SSVertex(position: vector_float4(0.5, -0.5, 0, 1), color: vector_float4(0.0, 0.0, 1.0, 1.0))]
+            SSVertex(position: vector_float4(-0.5, -0.5, 0, 1), color: vector_float4(0.0, 1.0, 0.0, 1.0)),
+            SSVertex(position: vector_float4(0.5, -0.5, 0, 1), color: vector_float4(0.0, 0.0, 1.0, 1.0))]
         _vertexBuffer = _device.newBufferWithBytes(triangle, length: triangle.count * sizeof(SSVertex), options: MTLResourceOptions.CPUCacheModeDefaultCache)
     }
     
@@ -74,10 +74,10 @@ class SSMetalView: UIView {
         _metalLayer.frame = self.frame
         if let drawable = _metalLayer.nextDrawable() {
             let renderPassDesc = MTLRenderPassDescriptor()
-            renderPassDesc.colorAttachments[0].texture = drawable.texture
-            renderPassDesc.colorAttachments[0].loadAction = MTLLoadAction.Clear
+            renderPassDesc.colorAttachments[0].texture     = drawable.texture
+            renderPassDesc.colorAttachments[0].loadAction  = MTLLoadAction.Clear
             renderPassDesc.colorAttachments[0].storeAction = MTLStoreAction.Store
-            renderPassDesc.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1.0)
+            renderPassDesc.colorAttachments[0].clearColor  = MTLClearColorMake(0.5, 0.5, 0.5, 1.0)
             
             let commandQueue = _device.newCommandQueue()
             let commandBuffer = commandQueue.commandBuffer()
